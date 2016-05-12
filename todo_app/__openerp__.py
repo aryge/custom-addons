0d29e8f6706db655dc245e2252028dfe616691fd
aryge <ary.gustanto@gmail.com>
2016-05-12 08:58:53 +0700

    upload isi folder awal

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'To Do Application',
+    'description': 'Manage your personal task with this module.',
+    'author': 'Daniel Reis',
+    'depends': ['mail'],
+    'application': True,
+    'data': ['todo_view.xml','security/ir.model.access.csv','security/todo_access_rules.xml',],
+}
